e2c0bc46bbf026d694b4f628a8d1ee3fe819c5d5
Cesar <crcg1995@hotmail.com>
2020-10-03 13:41:10 -0500

    Added new features for VOC2012 like dataset, you can create a VOC like dataset from a csv.
    Added BoundingB method to create Background annotations.
    
    Added method to perform data augmentation (rotation) for images with annotations.

--- Tools/Annotations/Reformat.py
+++ Tools/Annotations/Reformat.py
@@ -2,12 +2,14 @@ import cv2
 from lxml.etree import Element, SubElement, tostring
 import pandas as pd
 import os
-import json
 from shutil import copyfile
 from sklearn.model_selection import train_test_split
+import numpy as np
+import copy
+import imutils
 
 
-def csv2xml(data: pd.DataFrame):
+def csv2xml(data: pd.DataFrame, PathToAppend: str = "./", DatasetName: str = "", seed=32):
     """
     csv2xml iterates over a dataframe to create VOC annotations from a csv with the following format
 
@@ -16,15 +18,26 @@ def csv2xml(data: pd.DataFrame):
     ############################################### \\
 
     :param data: Dataframe containing the data
-    :param PathToAppend: Path to append to the path
+    :param PathToAppend: Path to append to creation of the dataset
+    :param DatasetName: Name of the dataset
     :return:
+
+    Creates the following structure in PathToAppend
+    ./VOC2012
+       |-------> Annotations (xml individual files)
+       |-------> ImageSets   (Separation for training, validation and test in txt)
+       |-------> JPEGImages  (Images to new folder)
     """
-    os.mkdir("./VOCdevkit")
-    os.mkdir("./VOCdevkit/VOC2012")
-    os.mkdir("./VOCdevkit/VOC2012/Annotations")
-    os.mkdir("./VOCdevkit/VOC2012/ImageSets")
-    os.mkdir("./VOCdevkit/VOC2012/ImageSets/Main")
-    os.mkdir("./VOCdevkit/VOC2012/JPEGImages")
+
+    ## Creation of new directories
+    main_directory = os.path.join(PathToAppend, "VOC2012")
+    os.mkdir(main_directory)
+    os.mkdir(os.path.join(main_directory, "Annotations"))  ## Annotations directory
+    os.mkdir(
+        os.path.join(main_directory, "ImageSets"))  ## ImageSets directory (Files with Training,Validation and Test)
+    os.mkdir(os.path.join(main_directory, "JPEGImages"))  ## Saved images
+
+    ## DataFrame manipulation
     Files = set(data['Filename'])  ## Extract unique Filenames
     Files = [x for x in Files]  ## Convert it to a list to iterate
 
@@ -32,19 +45,19 @@ def csv2xml(data: pd.DataFrame):
     Classes = [Class for Class in Classes]
     ImageSet = {}
     for Class in Classes:
-      ImageSet[Class] = []
+        ImageSet[Class] = []
 
     for Image in Files:  ## For every Image (DataFrame) there are several bounding boxes
         path = os.path.split(Image)
 
         node_root = Element('annotation')  ## Base node
         node_folder = SubElement(node_root, 'folder')  ## Folder node
-        node_folder.text = "VOC2012"
+        node_folder.text = main_directory
         node_filename = SubElement(node_root, 'filename')  ## Filename node
         node_filename.text = path[-1]
         node_source = SubElement(node_root, 'source')  ## Source node
         node_sourceDatabase = SubElement(node_source, 'database')  ## Source database
-        node_sourceDatabase.text = "Neovessels OPTOS"
+        node_sourceDatabase.text = DatasetName
         node_sourceAnnotation = SubElement(node_source, 'annotation')  ## Source annotation
         node_sourceAnnotation.text = "PASCAL VOC2012"
         node_sourceImage = SubElement(node_source, 'image')  ## Source image
@@ -54,7 +67,7 @@ def csv2xml(data: pd.DataFrame):
         I = cv2.imread(Image)
         [M, N, C] = I.shape
         # Move the image to ImageSets
-        copyfile(Image, os.path.join("./VOCdevkit/VOC2012/JPEGImages", path[-1]))
+        copyfile(Image, os.path.join(os.path.join(main_directory, "JPEGImages"), path[-1]))
 
         node_size = SubElement(node_root, 'size')
 
@@ -76,7 +89,7 @@ def csv2xml(data: pd.DataFrame):
             node_Odifficult = SubElement(node_object, 'difficult')
 
             ## Append to file for txt
-            ImageSet[bb['class']].append(path[-1]) ## We append the name of the image given a class
+            ImageSet[bb['class']].append(path[-1])  ## We append the name of the image given a class
 
             node_Oname.text = bb['class']
             node_Opose.text = "Unspecified"
@@ -96,10 +109,10 @@ def csv2xml(data: pd.DataFrame):
         ## Final node
         s = tostring(node_root, pretty_print=True)
         name = path[-1].split('.')[0]
-        with open(os.path.join("./VOCdevkit/VOC2012/Annotations", name + ".xml"), 'wb') as f:
+        with open(os.path.join(os.path.join(main_directory, "Annotations"), name + ".xml"), 'wb') as f:
             f.write(s)
 
-    #Indexes for train,trainval and val
+    # Indexes for train,trainval and val
     train = []
     trainval = []
     val = []
@@ -108,31 +121,179 @@ def csv2xml(data: pd.DataFrame):
     for Class in Classes:
         names = set(ImageSet[Class])
         names = [n for n in names]
-        X_train, X_test = train_test_split(names, test_size=0.2)
+        X_train, X_test = train_test_split(names, test_size=0.2, random_state=seed)
+        # We perform split again in the training set
+        X_train, X_validation = train_test_split(X_train, test_size=0.2, random_state=seed)
         X_train = [X.split('.')[0] for X in X_train]
+        X_validation = [X.split('.')[0] for X in X_validation]
         X_test = [X.split('.')[0] for X in X_test]
-        with open(f"./VOCdevkit/VOC2012/ImageSets/Main/{Class}_train.txt","x") as f:
+        with open(os.path.join(os.path.join(main_directory, "ImageSets"), f"{Class}_train.txt"), "x") as f:
             for item in X_train:
                 f.write("%s\n" % item)
-        with open(f"./VOCdevkit/VOC2012/ImageSets/Main/{Class}_trainval.txt", "x") as f:
-            for item in X_test:
+        with open(os.path.join(os.path.join(main_directory, "ImageSets"), f"{Class}_trainval.txt"), "x") as f:
+            for item in X_validation:
                 f.write("%s\n" % item)
-        with open(f"./VOCdevkit/VOC2012/ImageSets/Main/{Class}_val.txt", "x") as f:
+        with open(os.path.join(os.path.join(main_directory, "ImageSets"), f"{Class}_val.txt"), "x") as f:
             for item in X_test:
                 f.write("%s\n" % item)
 
         train = train + X_train
-        trainval = trainval + X_test
+        trainval = trainval + X_validation
         val = val + X_test
     # Write train, trainval and val txt
-    with open(f"./VOCdevkit/VOC2012/ImageSets/Main/train.txt","x") as f:
+    with open(os.path.join(os.path.join(main_directory, "ImageSets"), "train.txt"), "x") as f:
         for item in train:
-                f.write("%s\n" % item)
-    with open(f"./VOCdevkit/VOC2012/ImageSets/Main/trainval.txt", "x") as f:
+            f.write("%s\n" % item)
+    with open(os.path.join(os.path.join(main_directory, "ImageSets"), "trainval.txt"), "x") as f:
         for item in trainval:
-                f.write("%s\n" % item)
-    with open(f"./VOCdevkit/VOC2012/ImageSets/Main/val.txt", "x") as f:
+            f.write("%s\n" % item)
+    with open(os.path.join(os.path.join(main_directory, "ImageSets"), "val.txt"), "x") as f:
         for item in val:
-                f.write("%s\n" % item)
+            f.write("%s\n" % item)
 
-    ## Create label_map_json_path manually
+    print(""" Create label_map_json_path manually
+ item
+ {
+     name: "cat"
+     id: 1
+ }
+ item
+ {
+     name: "dog"
+     id: 2
+ }
+ item
+ {
+     name: "fox"
+     id: 3
+ }
+ item
+ {
+     name: "squirrel"
+     id: 4
+ }""")
+
+
+def augment(img_data, random_rot=False, horizontal_flips=False, vertical_flips=False, augment=True):
+    """ This function takes three parameters to define if data augmentation will be performed in the image given a
+    set of parameters. It has no effect if {augmented} is set to False
+
+        :param img_data
+        The data looks like a data structure (dictionary) that MUST contain the keys:
+         - filepath
+         - bboxes
+         - width
+         - height
+
+        Data augmentation is performed in a way that the transformation is done only in a 90 degree multiple.
+        A hard copy of the DataFrame is made and the coordinates are changed.
+
+        NOTE: an implementation of (-x to x) degrees must be done.
+
+        At the end the function returns the loaded image and the modified DataFrame.
+    """
+    assert 'filepath' in img_data
+    assert 'bboxes' in img_data
+    assert 'width' in img_data
+    assert 'height' in img_data
+
+    ## Copy original dictionary to change coordinates
+    img_data_aug = copy.deepcopy(img_data)
+
+    ## Correction to be read as RGB
+    img = cv2.imread(img_data_aug['filepath'])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    if augment:
+        rows, cols = img.shape[:2]
+        Applied_aug = "Augmented"
+
+        # Random horizontal rotation
+        if horizontal_flips and np.random.randint(0, 2) == 0:
+            img = cv2.flip(img, 1)
+            Applied_aug += "H"
+            for bbox in img_data_aug['bboxes']:
+                x1 = bbox['x1']
+                x2 = bbox['x2']
+                bbox['x2'] = cols - x1
+                bbox['x1'] = cols - x2
+        # Random vertical rotation
+        if vertical_flips and np.random.randint(0, 2) == 0:
+            img = cv2.flip(img, 0)
+            Applied_aug += "V"
+            for bbox in img_data_aug['bboxes']:
+                y1 = bbox['y1']
+                y2 = bbox['y2']
+                bbox['y2'] = rows - y1
+                bbox['y1'] = rows - y2
+
+        # If rotation allowed
+        if random_rot:
+            ## Rotate the image first from -15 to 15 degrees, limit this so the bounding box doesn't have a big area
+            angle = np.random.randint(-15, 15, 1)[0]
+            img = imutils.rotate(img.copy(), angle)
+            if angle < 0:
+                Applied_aug += "R" + str(np.abs(angle))[1:]
+            else:
+                Applied_aug += "R" + str(angle)
+
+            for bbox in img_data_aug['bboxes']:
+                if angle == 0:
+                    pass
+                new_bbox = bb_rot(angle, img, bbox)
+                bbox['x1'] = new_bbox['x1']
+                bbox['x2'] = new_bbox['x2']
+                bbox['y1'] = new_bbox['y1']
+                bbox['y2'] = new_bbox['y2']
+
+        # Change name with data augmented values
+        path = img_data_aug['filepath']
+        s = img_data_aug['filepath'].split('.')
+        path = path[:-len(s[-1]) - 1] + Applied_aug + "." + s[
+            -1]  # Append the applied augmentation before the extension
+
+    img_data_aug['height'] = img.shape[0]
+    img_data_aug['width'] = img.shape[1]
+
+    # Returns the modified structure and the modified image
+    return img_data_aug, img
+
+
+def bb_rot(angle, I, bb):
+    """
+    bb_rot rotates the coordinates of a bounding box with coordinates (x1,y1),(x2,y2)
+    :param angle  Is the angle in which the image was rotated
+    :param I      Is the original image
+    :param bb     Is the old bounding box with the coordinates
+    """
+    # Matrix of rotation for the points
+    rad = np.deg2rad(angle)
+    mat = [[np.cos(rad), -np.sin(rad)], [np.sin(rad), np.cos(rad)]]
+
+    # Shape of the image to make corrections in the coordinates
+    M, N, C = I.shape
+
+    """Due to the deformation for the rotation the new bounding box must be computed out
+       of the four points of the original bounding box. In a clockwise"""
+    ## 1
+    P1 = (np.dot(mat, np.array([[bb['x1'] - N // 2], [-bb['y1'] + M // 2]])))
+    P1 = np.array([int(P1[0] + N // 2), int(-P1[1] + M // 2)])
+    ## 2
+    P2 = (np.dot(mat, np.array([[bb['x2'] - N // 2], [-bb['y1'] + M // 2]])))
+    P2 = np.array([int(P2[0] + N // 2), int(-P2[1] + M // 2)])
+    ## 3
+    P3 = (np.dot(mat, np.array([[bb['x2'] - N // 2], [-bb['y2'] + M // 2]])))
+    P3 = np.array([int(P3[0] + N // 2), int(-P3[1] + M // 2)])
+    ## 4
+    P4 = (np.dot(mat, np.array([[bb['x1'] - N // 2], [-bb['y2'] + M // 2]])))
+    P4 = np.array([int(P4[0] + N // 2), int(-P4[1] + M // 2)])
+
+    ## New bounding box
+    new_bb = {}
+    ## Look for the smallest and greates values
+    new_bb['x1'] = np.min([P1[0], P2[0], P3[0], P4[0]])
+    new_bb['x2'] = np.max([P1[0], P2[0], P3[0], P4[0]])
+    new_bb['y1'] = np.min([P1[1], P2[1], P3[1], P4[1]])
+    new_bb['y2'] = np.max([P1[1], P2[1], P3[1], P4[1]])
+
+    return new_bb
---
+++ Tools/Generators/BoundingB.py
@@ -0,0 +1,86 @@
+import xml.etree.ElementTree as ET
+import copy
+import numpy as np
+from ..Generators.Binary_generator import bb_intersection_over_union as bbIoU
+
+
+class VOC_format:
+    def __init__(self, xml_file: str):
+        # Filename
+        self.name = xml_file
+        # Extract the root
+        self.root = ET.parse(xml_file).getroot()
+        self.update_dependencies()
+
+    def createBackgroundBB(self, n=1):
+        # Extract all the bounding boxes
+        bboxes = [obj[-1] for obj in self.objects if obj[-1].tag == "bndbox"]
+        # The means of the bounding boxes will be the height and width of the background bounding boxes
+        width_mean = sum([float(x[2].text) - float(x[0].text) for x in bboxes]) / len(
+            bboxes)  # Extract all the widths and compute mean
+        height_mean = sum([float(x[3].text) - float(x[1].text) for x in bboxes]) / len(
+            bboxes)  # Extract all the heights and compute mean
+        # Loop over the number of bounding boxes to create
+        for i in range(n):
+            IoU = 1.0
+            counter = 0
+            while IoU > 0.01:  # While the Intersection over Union is too big we will create new coordinates
+                xmin = float(np.random.randint(0, self.width - width_mean, 1)[0])
+                xmax = xmin + width_mean
+                ymin = float(np.random.randint(0, self.height - height_mean, 1)[0])
+                ymax = ymin + height_mean
+                # Compute all the IoU with the other bounding boxes
+                IoU = sum([bbIoU([xmin, ymin, xmax, ymax],
+                                 [float(x[0].text), float(x[1].text), float(x[2].text), float(x[3].text)]) for x in
+                           bboxes])
+                counter += 1
+                if counter > 99:
+                    # If after 100 tries no bounding box is created then it is highly possible that there is no space left for a BackgroundBB
+                    print("After {} tries no Background Bounding Box was created".format(counter))
+                    print("{} BackgroundBB created".format(i + 1))
+                    self.saveXml()
+                    return None
+            # Once the IoU is small enough then we can add that bounding box as an object to root
+            obj = copy.deepcopy(self.objects[-1])
+            obj[0].text = "Background"  # Name
+            obj[1].text = "Unspecified"  # Pose
+            obj[2].text = "0"  # Truncated
+            obj[3].text = "0"  # Difficult
+            # Add coordinates to object
+            obj[4][0].text = str(xmin).split('.')[0]  # Keep only the integer part
+            obj[4][1].text = str(ymin).split('.')[0]
+            obj[4][2].text = str(xmax).split('.')[0]
+            obj[4][3].text = str(ymax).split('.')[0]
+
+            # Append new object to root
+            self.root.append(obj)
+            # Compute objects and bounding boxes again for new IoU
+            self.update_dependencies()
+            bboxes = [obj[-1] for obj in self.objects if obj[-1].tag == "bndbox"]
+        print("{} BackgroundBB created".format(i + 1))
+        self.saveXml()
+
+    def saveXml(self):
+        """This function saves the changes and overwrites the xml file"""
+        with open(self.name, "wb") as f:
+            f.write(ET.tostring(self.root))
+
+    def update_dependencies(self):
+        """This function updates all the variables of the object"""
+        # Extract folder
+        self.folder = [x for x in self.root if x.tag == "folder"][0]
+        # Extract filename
+        self.filename = [x for x in self.root if x.tag == "filename"][0]
+        # Extract source
+        self.source = [x for x in self.root if x.tag == "source"][0]
+        # Extract size
+        self.size = [x for x in self.root if x.tag == "size"][0]
+        self.width = int([x for x in self.size if x.tag == "width"][0].text)
+        self.height = int([x for x in self.size if x.tag == "height"][0].text)
+        # Extract objects
+        self.objects = [x for x in self.root if x.tag == "object"]
+        # Extract segmented
+        self.segmented = [x for x in self.root if x.tag == "segmented"]
+        # Compute classes
+        self.classes = [x[0].text for x in self.objects]
+
