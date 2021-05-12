46602ea6fb3cf169894f49b2dfe25830418a81ce
crcg1995@hotmail.com <123456789Ab>
2021-05-12 09:49:34 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Segmentation_OPTOS',
+    version='0.1',
+    packages=['pytorch'],
+    url='https://github.com/Vengadore/Segmentation_OPTOS',
+    license='',
+    author='César Rolando Carrillo Gómez',
+    author_email='crcg1995@hotmail.com',
+    description='Tools for image segmentation'
+)
